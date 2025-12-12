2e119d9f7f9fab5ac8f30029a7d8ed7b6f9a2fb4
Ali Syed <alisyed0206@gmail.com>
2025-12-11 19:26:21 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ali Syed on 2025-12-11.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
