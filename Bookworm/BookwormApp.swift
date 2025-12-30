130ad94b18a16215e8ca312b2e26c9800e54599a
Ali Syed <alisyed0206@gmail.com>
2025-12-30 02:57:16 -0500

    learning about SwiftData

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Ali Syed on 2025-12-11.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Student.self)
     }
 }
